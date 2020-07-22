de44d8751667fbe6dd169a0f6506e5d8bc9086a3
Nicholas Nhien <nnhien@protonmail.com>
2020-07-22 14:14:46 -0700

    add catalog parser

--- course.py
+++ course.py
@@ -1,9 +1,10 @@
 class Course:
     def __init__(self):
         self.dept = ''
+        self.title = ''
         self.ctlg_no = ''
         self.desc = ''
-        self.units = 0.0
+        self.units = ''
 
     def __str__(self):
         return str(self.__class__) + ": " + str(self.__dict__)
