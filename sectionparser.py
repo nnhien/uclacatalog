14ef65f70ccb1fa3cfe9829cf935f4de228cb6f7
Nicholas Nhien <nnhien@protonmail.com>
2020-07-22 22:07:55 -0700

    improve seperation of concerns for section parser

--- sectionparser.py
+++ sectionparser.py
@@ -12,40 +12,53 @@ import requests as req
 import re
 import time
 
-def parse_sections(resp) -> List[Section]:
+def parse_sections(resp, course) -> List[Section]:
+    out = []
+    out.extend(_parse_lectures(resp, course))
+    return out
+
+# For our purposes, lectures will be treated the same as labs, since they contain the same metadata
+def _parse_lectures(resp, course):
     out = []
     sections_soup = BeautifulSoup(resp.text, 'lxml').find('div', {'id': re.compile('\\d*-children')})
     sections_arr = sections_soup.find_all("div", class_="class-info")
     for section_soup in sections_arr:
         section = Section()
-        _parse_id(section, section_soup)
-        _parse_status(section, section_soup)
-        _parse_time(section, section_soup)
-        _parse_location(section, section_soup)
-        _parse_instructors(section, section_soup)
+        section.id = _parse_id(section_soup)
+        section.is_open = _parse_openness(section_soup)
+        section.enrolled = _parse_enrollment(section_soup)
+        section.enrolled_max = _parse_enrollment_max(section_soup)
+        section.waitlisted = _parse_waitlisted(section_soup)
+        section.waitlisted_max = _parse_waitlisted_max(section_soup)
+        section.meet_days = _parse_days(section_soup)
+        section.start_time = str(_parse_start(section_soup))
+        section.end_time = str(_parse_end(section_soup))
+        section.location = _parse_location(section_soup)
+        section.instructors = _parse_instructors(section_soup)
         section.last_updated = int(time.time())
-        out.append(section)
+        section.course = course
+        out.append(course)
     return out
 
-def _parse_id(section, section_soup):
-    # ID Attribute is in format of ID_subjAreaCLASSNUM; we want to split at '_' and take the first element
-    section.id = section_soup.div['id'].split("_")[0]
+def _fetch_discussion_sections():
+     NotImplemented
 
-def _parse_status(section, section_soup):
-    # Collect relevant columns (UCLA for some reason doesn't include waitlist with status, but we will parse it in the same function for simplicity)
+def _fetch_final():
+    NotImplemented
+
+def _parse_id(section_soup):
+    # ID Attribute is in format of ID_subjAreaCLASSNUM; we want to split at '_' and take the first element
+    return section_soup.div['id'].split("_")[0]
+
+def _match_status(section_soup):
     status = section_soup.find("div", class_="statusColumn")
-    waitlist = section_soup.find("div", class_="waitlistColumn")
 
     # Use regex match groups to seperate openness from class capacity
-    groups = re.findall("(Open|Closed|Waitlist)((\\d+ of \\d+ Enrolled)|(Class Full \\(\\d+\\)))", status.text)[0]
+    return re.findall("(Open|Closed|Waitlist)((\\d+ of \\d+ Enrolled)|(Class Full \\(\\d+\\)))", status.text)[0]
 
-    # Do parsing
-    _parse_openness(section, groups[0])
-    _parse_enrollment(section, groups[1])
-    parse_waitlist(section, waitlist.text)
-
-def _parse_openness(section, open):
-    section.is_open = open == "Open"
+def _parse_openness(section_soup):
+    status = _match_status(section_soup)[0]
+    return status == "Open"
 
 ''' 
 For matching enrollment and waitlist, we only care about the numbers for matching. 
@@ -53,47 +66,58 @@ Also, there will always be an integer to match for (namely, 0)
 
 The returned tuple always has the structure (current_amt, max_cap)
 '''
-def _parse_enrollment(section, enrollment):
-    groups = re.findall("\\d+", enrollment)
-    section.enrolled = groups[0]
-    if len(groups) > 1:
-        section.enrolled_max = groups[1]
-    else:
-        section.enrolled_max = groups[0]
+def _parse_enrollment(section_soup):
+    enrollment = _match_status(section_soup)[1]
+    groups = re.findall('\\d+', enrollment)
+    return groups[0]
 
-def parse_waitlist(section, waitlist):
-    groups = re.findall("\\d+", waitlist)
-    section.waitlisted = groups[0]
+def _parse_enrollment_max(section_soup):
+    enrollment = _match_status(section_soup)[1]
+    groups = re.findall('\\d+', enrollment)
     if len(groups) > 1:
-        section.waitlisted_max = groups[1]
+        return groups[1]
     else:
-        section.waitlisted_max = groups[0]
+        return groups[0]
 
-def _parse_time(section, section_soup):
+def _match_waitlisted(section_soup):
+    waitlist = section_soup.find("div", class_="waitlistColumn")
+    return re.findall("\\d+", waitlist.text)
+
+def _parse_waitlisted(section_soup):
+    groups = _match_waitlisted(section_soup)
+    return groups[0]
+
+def _parse_waitlisted_max(section_soup):
+    groups = _match_waitlisted(section_soup)
+    if len(groups) > 1:
+        return groups[1]
+    else:
+        return groups[0]
+
+def _parse_time(section_soup):
     time_soup = section_soup.find("div", class_="timeColumn")
 
     # Text comes in the format DAYS \n TIME, so we split it to give an array of [DAYS, TIME]
-    time = time_soup.text.split()
-    _parse_days(section, time[0])
-    _parse_start_end(section, time[1])
+    return time_soup.text.split()
 
-def _parse_days(section, days):
+def _parse_days(section_soup):
+    days = _parse_time(section_soup)[0]
     groups = re.findall("(M+)?(T+)?(W+)?(R+)?(F+)?", days)[0]
     
-    # I'm bad at regex so filter for elements that aren't empty strings
-    days = list(filter(lambda e: e != '', groups))
-    section.meet_days.extend(days)
+    # Filter for elements that aren't empty strings
+    return list(filter(lambda e: e != '', groups))
 
-def _parse_start_end(event, time):
-    time_arr = time.split('-') # time_arr is in format of [start_time, end_time]
-    _parse_start(event, time_arr[0])
-    _parse_end(event, time_arr[1])
+def _parse_start_end(section_soup):
+    time = _parse_time(section_soup)[1]
+    return time.split('-') # time_arr is in format of [start_time, end_time]
 
-def _parse_start(event, time_str):
-    event.start_time = str(_parse_time_str(time_str))
+def _parse_start(section_soup):
+    start_str = _parse_start_end(section_soup)[0]
+    return _parse_time_str(start_str)
 
-def _parse_end(event, time_str):
-    event.end_time = str(_parse_time_str(time_str))
+def _parse_end(section_soup):
+    end_str = _parse_start_end(section_soup)[1]
+    return _parse_time_str(end_str)
 
 def _parse_time_str(time_str):
     if time_str.find(':') > -1:
@@ -103,10 +127,9 @@ def _parse_time_str(time_str):
         # Time *is* the hour; for some reason UCLA doesn't standardize timestamps and assumes that 12:00 = 12. We will follow this assumption grudgingly.
         return datetime.strptime(time_str, '%I%p')
 
-def _parse_location(section, section_soup):
-    location = section_soup.find("div", class_="locationColumn").text
-    section.location = location.strip()
+def _parse_location(section_soup):
+    return section_soup.find("div", class_="locationColumn").text.strip()
 
-def _parse_instructors(section, section_soup):
+def _parse_instructors(section_soup):
     instructor_soup = section_soup.find("div", class_="instructorColumn")
-    section.instructors = [x for x in instructor_soup.p.contents if getattr(x, 'name', None) != 'br']
+    return [x for x in instructor_soup.p.contents if getattr(x, 'name', None) != 'br']
