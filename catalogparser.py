50c25d1206e8a68d1438fdb54d91573017cadf02
Nicholas Nhien <nnhien@protonmail.com>
2020-07-22 17:00:50 -0700

    add registrar catalog numbering convention support

--- catalogparser.py
+++ catalogparser.py
@@ -7,6 +7,7 @@ from typing import List
 from course import Course
 from bs4 import BeautifulSoup
 import api
+import re
 
 def parse_catalog(resp: str, subj_area: str, div: str) -> List[Course]:
     if div == api.ALL_DIV: 
@@ -28,24 +29,50 @@ def _parse_course_list(resp, subj_area, div):
     for course_soup in courses_soup:
         course = Course()
         course.dept = subj_area
-        course.title = _parse_course_title(course_soup)
-        course.ctlg_no = _parse_course_ctlg_no(course_soup)
         course.units = _parse_course_units(course_soup)
         course.desc = _parse_course_desc(course_soup)
+        _parse_head(course, course_soup)
         course_list.append(course)
     return course_list
         
-'''
-Title and catalog numbers are enclosed in one h3 tag; for readability of code however, we will
-seperate the parsing of the two fields
 
-Format on the UCLA page is always 'CTLG_NO. TITLE'
 '''
-def _parse_course_title(course_soup):
-    return course_soup.h3.text.split('. ')[1]
+Title and catalog numbers are enclosed in one h3 tag.
 
-def _parse_course_ctlg_no(course_soup):
-    return course_soup.h3.text.split('. ')[0]
+The Registrar specifies course numbering conventions at https://www.registrar.ucla.edu/Academics/Course-Descriptions/Course-Numbering-and-Description-Guide
+Noteably:
+- Courses that are credited concurrently between undergrad and grad are prefaced with C
+- Courses that are administered between different departments are prefaced with M
+- Courses that are labs (for engineering) are appended with L
+- Courses that are individual study are appended with S, but if it's part of a series it's put before the series letter
+- C always comes before M
+
+Therefore, the longest course number is CM999SCL, but I haven't been able to find one with all the conventions.
+'''
+def _parse_head(course, course_soup):
+    head = course_soup.h3.text.split('. ')
+    ctlg_no = head[0]
+    ctlg_no_components = re.findall('(C?)(M?)(\\d+[^L]+)(L?)', ctlg_no)[0]
+    course.title = _extract_course_title(head)
+    course.is_concurrent = _extract_is_concurrent(ctlg_no_components)
+    course.is_multi_listed = _extract_is_multi_listed(ctlg_no_components)
+    course.ctlg_no = _extract_ctlg_no(ctlg_no_components)
+    course.is_lab = _extract_is_lab(ctlg_no_components)
+    
+def _extract_course_title(head):
+    return head[1]
+
+def _extract_is_concurrent(ctlg_no_comp):
+    return ctlg_no_comp[0] != ''
+
+def _extract_is_multi_listed(ctlg_no_comp):
+    return ctlg_no_comp[1] != ''
+
+def _extract_ctlg_no(ctlg_no_comp):
+    return ctlg_no_comp[2]
+
+def _extract_is_lab(ctlg_no_comp):
+    return ctlg_no_comp[3] != ''
 
 '''
 Course units is not always an integer (sometimes the registrar specifies a range like '1.0 to 4.0')
--- course.py
+++ course.py
@@ -1,10 +1,20 @@
 class Course:
     def __init__(self):
         self.dept = ''
-        self.title = ''
         self.ctlg_no = ''
+        self.title = ''
         self.desc = ''
         self.units = ''
+        self.is_concurrent = False
+        self.is_multi_listed = False
+        self.is_lab = False
+        self.token = ''
+
+    def set_token(self):
+        if self.dept == '' or self.ctlg_no == '':
+            raise ValueError
+        else:
+            NotImplemented
 
     def __str__(self):
         return str(self.__class__) + ": " + str(self.__dict__)
