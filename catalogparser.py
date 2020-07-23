ecef3ab945ecf8a4c2b2c1d4eea61b1581fd492a
Nicholas Nhien <nnhien@protonmail.com>
2020-07-23 21:20:57 +0000

    move comment

--- catalogparser.py
+++ catalogparser.py
@@ -1,13 +1,13 @@
-'''
-Parses the response from https://www.registrar.ucla.edu/Academics/Course-Descriptions/Course-Details after sending a
-request containing the subject area 
-'''
-
 from typing import List
 from course import Course
 from bs4 import BeautifulSoup
 import api
 import re
+
+'''
+Parses the response from https://www.registrar.ucla.edu/Academics/Course-Descriptions/Course-Details after sending a
+request containing the subject area 
+'''
 
 def parse_catalog(resp: str, subj_area: str, div: str) -> List[Course]:
     if div == api.ALL_DIV: 
