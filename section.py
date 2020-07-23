b5f4b502b98fdd5abdae907317bda6885fc693a7
Nicholas Nhien <nnhien@protonmail.com>
2020-07-22 23:42:31 -0700

    add json encoding support for sections

--- section.py
+++ section.py
@@ -1,5 +1,6 @@
 from course import Course
 import base64
+import json
 
 class Event:
     def __init__(self):
@@ -16,6 +17,8 @@ class Section(Event):
     def __init__(self):
         super().__init__()
         self.id = 0
+        self.sec_no = 0
+        self.term = ''
         self.enrollable = False
         self.waitlistable = False
         self.enrolled = 0
@@ -25,6 +28,17 @@ class Section(Event):
         self.instructors = []
         self.last_updated = 0
         self.course = None
+
+    def to_jsons(self):
+        json_obj = json.loads(self.course.to_jsons(self.term))
+        json_obj['IsRoot'] = False
+        json_obj['SessionGroup'] = None
+        json_obj['ClassNumber'] = self.sec_no
+        json_obj['SequenceNumber'] = '1'
+        json_obj['Path'] = self._get_path()
+        json_obj['Token'] = self.get_token()
+
+        return json.dumps(json_obj)
 
     '''
     See course.py for full documentation on how the UCLA Registrar formats its tokens
@@ -42,10 +56,14 @@ class Section(Event):
         else:
             # I am going to commit heresy and call a private function outside of its intended scope, 
             # but I don't feel like reimplementing function. Sue me.
-            unencoded_token = self.course._get_full_ctlg_no() + self.id + "_" + self.course._get_path()
+            unencoded_token = self.course._get_full_ctlg_no() + self._get_path()
             unencoded_token_bytes = unencoded_token.encode('utf-8')
             base64_token = base64.standard_b64encode(unencoded_token_bytes)
             return base64_token.decode('utf-8')
+
+    def _get_path(self):
+        # This feels so dirty
+        return self.id + "_" + self.course._get_path()
 
     def __str__(self):
         return str(self.__class__) + ": " + str(self.__dict__)
