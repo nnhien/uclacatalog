8a87191e193866c8811b64e41207bfb059117e65
Nicholas Nhien <nnhien@protonmail.com>
2020-07-25 04:15:33 +0000

    Update setup to reflect license change

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Operating System :: OS Independent",
         "Topic :: Internet :: WWW/HTTP :: Indexing/Search"
     ],
