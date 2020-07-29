3b40324089b3348305caa08ac82d1f942e8eaa6c
Nicholas Nhien <nnhien@protonmail.com>
2020-07-29 15:40:07 -0700

    1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="uclacatalog",
-    version="1.0.1",
+    version="1.1.0",
     author="Nicholas Nhien",
     author_email="nknhien@ucla.edu",
     description="A Python library to retrieve course and section information from the UCLA Registrar",
