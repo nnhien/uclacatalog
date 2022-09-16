dee5d051236b586ec138d7fd474fb8a5e1f008bb
Vincent Lin <soulace2425@outlook.com>
2022-09-15 19:16:14 -0700

    Update package namespace to include new enums

--- uclacatalog/__init__.py
+++ uclacatalog/__init__.py
@@ -1,1 +1,1 @@
-from .api import ALL_DIV, LOWER_DIV, UPPER_DIV, GRAD_DIV, LEGAL_SA, fetch_catalog, fetch_matching_courses, fetch_sections
+from .api import ALL_DIV, LOWER_DIV, UPPER_DIV, GRAD_DIV, LEGAL_SA, Division, Subject, fetch_catalog, fetch_matching_courses, fetch_sections
